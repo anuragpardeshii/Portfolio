ef811ba7e44c2cf8212e8446d4689ed2af83d9f1
Anurag Pardeshi <pardeshianurag22@gmail.com>
2024-11-17 10:56:15 +0530

    CORS ALLOW ALL

--- backend/index.js
+++ backend/index.js
@@ -5,28 +5,22 @@ const cors = require('cors');
 
 const app = express();
 
-// CORS configuration
-app.use(cors({
-  origin: 'https://anuragpardeshiportfolio.vercel.app', // Corrected to match the domain without trailing slash
-  methods: ['POST', 'GET', 'OPTIONS'], // Allow OPTIONS preflight requests
-  credentials: true
-}));
-
 // Middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Handle preflight requests
-app.options('/api/send', cors());
+// CORS configuration
+const corsOptions = {
+  origin: '*', // Update to match your domain
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-// Middleware to set CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://anuragpardeshiportfolio.vercel.app'); // Update to match your domain
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('/api/send', cors(corsOptions));
 
 // Route to handle form submission
 app.post('/api/send', (req, res) => {
